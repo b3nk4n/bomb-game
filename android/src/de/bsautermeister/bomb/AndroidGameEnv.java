d092846f5d2f30260b2b70d9bdf57880beec6879
Benjamin Kan <Kan@ibm.com>
2023-12-31 13:38:44 -0800

    Fix minor compile/run issues on Android

--- android/src/de/bsautermeister/bomb/AndroidGameEnv.java
+++ android/src/de/bsautermeister/bomb/AndroidGameEnv.java
@@ -1,5 +1,7 @@
 package de.bsautermeister.bomb;
 
+import de.bsautermeister.bomb.game.BuildConfig;
+
 public class AndroidGameEnv implements GameEnv {
     @Override
     public String getVersion() {
